d4aec24da8e92a2fab71d742da54e271d63644c6
fseitz <fabi.seitz@gmail.com>
2024-12-31 15:39:26 +0100

    setup first basic example of manager

--- frontend/lib/src/manager/game_manager.dart
+++ frontend/lib/src/manager/game_manager.dart
@@ -1,12 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_command/flutter_command.dart';
 import 'package:tournament_manager/src/service/game_rest_api.dart';
 import 'package:watch_it/watch_it.dart';
 
-abstract class GameManager {}
+abstract class GameManager extends ChangeNotifier {
+  late Command<String, String> getGameDataCommand;
+}
 
-class GameManagerImplementation implements GameManager {
+class GameManagerImplementation extends ChangeNotifier implements GameManager {
   late final GameRestApi _gameRestApi;
+
+  @override
+  late Command<String, String> getGameDataCommand;
 
   GameManagerImplementation() {
     _gameRestApi = di<GameRestApi>();
+
+    getGameDataCommand = Command.createAsync(
+      (x) async {
+        return await _gameRestApi.getGameData();
+      },
+      initialValue: '',
+    );
   }
 }
