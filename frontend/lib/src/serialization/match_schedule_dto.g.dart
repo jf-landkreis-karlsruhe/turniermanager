450fbeef64461bf2b712019f425288f4ad3c2ffc
fseitz <fabi.seitz@gmail.com>
2024-12-31 15:56:36 +0100

    setup first basic example of serialization

---
+++ frontend/lib/src/serialization/match_schedule_dto.g.dart
@@ -0,0 +1,13 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'match_schedule_dto.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+MatchScheduleDto _$MatchScheduleDtoFromJson(Map<String, dynamic> json) =>
+    MatchScheduleDto();
+
+Map<String, dynamic> _$MatchScheduleDtoToJson(MatchScheduleDto instance) =>
+    <String, dynamic>{};
