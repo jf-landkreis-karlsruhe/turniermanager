93f253b255e31c0f0b50052c68c492de5a2895c5
fseitz <fabi.seitz@gmail.com>
2024-12-31 16:00:49 +0100

    fill schedule class with properties

--- frontend/lib/src/serialization/match_schedule_dto.dart
+++ frontend/lib/src/serialization/match_schedule_dto.dart
@@ -5,7 +5,21 @@ part 'match_schedule_dto.g.dart';
 
 @JsonSerializable()
 class MatchScheduleDto {
-  MatchScheduleDto();
+  MatchScheduleDto(
+    this.field,
+    this.team1,
+    this.pointsTeam1,
+    this.team2,
+    this.pointsTeam2,
+    this.startTime,
+  );
+
+  String field;
+  String team1;
+  int pointsTeam1;
+  String team2;
+  int pointsTeam2;
+  String startTime;
 
   factory MatchScheduleDto.fromJson(Map<String, dynamic> json) =>
       _$MatchScheduleDtoFromJson(json);
--- frontend/lib/src/serialization/match_schedule_dto.g.dart
+++ frontend/lib/src/serialization/match_schedule_dto.g.dart
@@ -7,7 +7,21 @@ part of 'match_schedule_dto.dart';
 // **************************************************************************
 
 MatchScheduleDto _$MatchScheduleDtoFromJson(Map<String, dynamic> json) =>
-    MatchScheduleDto();
+    MatchScheduleDto(
+      json['field'] as String,
+      json['team1'] as String,
+      (json['pointsTeam1'] as num).toInt(),
+      json['team2'] as String,
+      (json['pointsTeam2'] as num).toInt(),
+      json['startTime'] as String,
+    );
 
 Map<String, dynamic> _$MatchScheduleDtoToJson(MatchScheduleDto instance) =>
-    <String, dynamic>{};
+    <String, dynamic>{
+      'field': instance.field,
+      'team1': instance.team1,
+      'pointsTeam1': instance.pointsTeam1,
+      'team2': instance.team2,
+      'pointsTeam2': instance.pointsTeam2,
+      'startTime': instance.startTime,
+    };
