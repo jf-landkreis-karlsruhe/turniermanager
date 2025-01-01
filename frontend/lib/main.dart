7526fed6519d47b00adc66edc69d125fdc3b7791
fseitz <fabi.seitz@gmail.com>
2025-01-01 15:56:48 +0100

    use path strategy

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:tournament_manager/src/manager/game_manager.dart';
 import 'package:tournament_manager/src/service/game_rest_api.dart';
 import 'package:watch_it/watch_it.dart';
 import 'src/app.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 
 void setup() {
   // register REST API services
@@ -14,6 +15,8 @@ void setup() {
 
 void main() async {
   setup();
+
+  usePathUrlStrategy();
   // Run the app.
   runApp(const MainWidget());
 }
