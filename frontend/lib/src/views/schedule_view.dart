699518799da0ff0e2a28a41b6c355644e55d5b03
fseitz <fabi.seitz@gmail.com>
2025-01-01 20:26:32 +0100

    build league views

--- frontend/lib/src/views/schedule_view.dart
+++ frontend/lib/src/views/schedule_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tournament_manager/src/manager/game_manager.dart';
 import 'package:tournament_manager/src/model/league.dart';
+import 'package:tournament_manager/src/model/match_schedule_entry.dart';
 import 'package:watch_it/watch_it.dart';
 
 class ScheduleView extends StatelessWidget with WatchItMixin {
@@ -66,23 +67,75 @@ class LeagueView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Column(
-        children: [
-          SizedBox(
-            height: 20,
-            child: Row(
-              children: [
-                Text('Liga ${league.leagueNo}'),
-                const SizedBox(width: 10),
-                const Icon(Icons.gamepad),
-              ],
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 20,
+              child: Row(
+                children: [
+                  Text('Liga ${league.leagueNo}'),
+                  const SizedBox(width: 10),
+                  const Icon(Icons.gamepad),
+                ],
+              ),
             ),
-          ),
-          Expanded(
-            child: Text('Test'),
-          ),
-        ],
+            Expanded(
+              child: SizedBox(
+                width: 500,
+                child: ListView(
+                  children: league.scheduledGames
+                      .map((element) =>
+                          ScheduleEntry(matchScheduleEntry: element))
+                      .toList(),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
+    );
+  }
+}
+
+class ScheduleEntry extends StatelessWidget {
+  const ScheduleEntry({
+    super.key,
+    required this.matchScheduleEntry,
+  });
+
+  final MatchScheduleEntry matchScheduleEntry;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        SizedBox(
+          width: 150,
+          child: Row(
+            children: [
+              Text('Platz ${matchScheduleEntry.field}'),
+              const SizedBox(width: 5),
+              const Text('|'),
+              const SizedBox(width: 5),
+              Text(matchScheduleEntry.startTime),
+            ],
+          ),
+        ),
+        Expanded(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(matchScheduleEntry.team1),
+              const SizedBox(width: 5),
+              const Text(':'),
+              const SizedBox(width: 5),
+              Text(matchScheduleEntry.team2),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
