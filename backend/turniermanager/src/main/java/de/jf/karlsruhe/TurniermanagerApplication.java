01f98c622b2de26fb899c95e42183f5cde05e829
NilsHeinzelmann <123253152+NilsHeinzelmann@users.noreply.github.com>
2025-01-13 22:13:39 +0100

    Backend INIT
    Backend init

---
+++ backend/turniermanager/src/main/java/de/jf/karlsruhe/TurniermanagerApplication.java
@@ -0,0 +1,13 @@
+package de.jf.karlsruhe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurniermanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurniermanagerApplication.class, args);
+	}
+
+}
---
+++ backend/turniermanager/src/test/java/de/jf/karlsruhe/TurniermanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.jf.karlsruhe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurniermanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
