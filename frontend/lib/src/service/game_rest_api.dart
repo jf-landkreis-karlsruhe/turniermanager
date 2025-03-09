dda5e4ebf37e02770eefbec6b0cb3c4dfe1cb73f
fseitz <fabi.seitz@gmail.com>
2025-03-09 15:40:32 +0100

    extract test data to separate class

--- frontend/lib/src/service/game_rest_api.dart
+++ frontend/lib/src/service/game_rest_api.dart
@@ -45,49 +45,6 @@ class GameRestApiImplementation extends RestClient implements GameRestApi {
 
   @override
   Future<MatchScheduleDto?> getSchedule(String ageGroup) async {
-    //TODO: remove test data
-    int fieldCount = 1;
-    int teamCount = 1;
-    int hourCount = 10;
-    int timeCount = 10;
-
-    var scheduleList = List.generate(
-      10,
-      (innerIndex) {
-        var result = MatchScheduleEntryDto(
-          "$fieldCount",
-          "team${teamCount++}",
-          "team${teamCount++}",
-          "$hourCount:$timeCount Uhr",
-        );
-
-        fieldCount++;
-        if (fieldCount > 3) {
-          fieldCount = 1;
-        }
-
-        if (teamCount > 4) {
-          teamCount = 1;
-        }
-
-        timeCount += 10;
-        if (timeCount >= 60) {
-          timeCount = 10;
-          hourCount++;
-        }
-
-        return result;
-      },
-    );
-
-    return MatchScheduleDto(1)
-      ..leagueSchedules = List.generate(
-        8,
-        (index) {
-          return LeagueDto(index + 1)..scheduledGames = scheduleList;
-        },
-      );
-
     final uri = getScheduleUri.replace(
       queryParameters: {
         'ageGroup': ageGroup,
@@ -106,39 +63,6 @@ class GameRestApiImplementation extends RestClient implements GameRestApi {
 
   @override
   Future<ResultsDto?> getResults(String ageGroup) async {
-    //TODO: remove test data
-
-    var resultList = List.generate(
-      10,
-      (index) {
-        var randomGenerator = Random(index);
-        var result = ResultEntryDto(
-          'Team$index',
-          randomGenerator.nextInt(100),
-          randomGenerator.nextInt(100),
-          randomGenerator.nextInt(100),
-          randomGenerator.nextInt(100),
-          randomGenerator.nextInt(100),
-          randomGenerator.nextInt(100),
-        );
-
-        return result;
-      },
-    );
-
-    resultList.sort(
-      (a, b) => a.points.compareTo(b.points),
-    );
-
-    return ResultsDto(1)
-      ..leagueResults = List.generate(
-        3,
-        (index) {
-          return resultleague.LeagueDto(index + 1)
-            ..gameResults = resultList.reversed.toList();
-        },
-      );
-
     final uri = getResultsUri.replace(
       queryParameters: {
         'ageGroup': ageGroup,
@@ -183,8 +107,44 @@ class GameRestApiImplementation extends RestClient implements GameRestApi {
   }
 
   @override
-  Future<RoundDto?> getCurrentRound() async {
+  Future<List<AgeGroupDto>> getAllAgeGroups() async {
+    final response = await client.get(getAllAgeGroupsUri, headers: headers);
+
+    if (response.statusCode == 200) {
+      var json = jsonDecode(response.body);
+
+      if (json is List<Map<String, dynamic>>) {
+        return json.map((e) => AgeGroupDto.fromJson(e)).toList();
+      }
+    }
+
+    return [];
+  }
+
+  @override
+  Future<RoundDto?> getCurrentRound() {
     // TODO: implement getCurrentRound
+    throw UnimplementedError();
+  }
+}
+
+class GameTestRestApi extends GameRestApi {
+  @override
+  Future<bool?> endCurrentGames() async {
+    return true;
+  }
+
+  @override
+  Future<List<AgeGroupDto>> getAllAgeGroups() async {
+    return [
+      AgeGroupDto('Altersklasse 1'),
+      AgeGroupDto('Altersklasse 2'),
+      AgeGroupDto('Altersklasse 3'),
+    ];
+  }
+
+  @override
+  Future<RoundDto?> getCurrentRound() async {
     return RoundDto("Runde 1")
       ..games = [
         GameDto(
@@ -247,27 +207,91 @@ class GameRestApiImplementation extends RestClient implements GameRestApi {
   }
 
   @override
-  Future<List<AgeGroupDto>> getAllAgeGroups() async {
-    return getAllAgeGroupsTestData(); // TODO: remove test data
+  Future<ResultsDto?> getResults(String ageGroup) async {
+    var resultList = List.generate(
+      10,
+      (index) {
+        var randomGenerator = Random(index);
+        var result = ResultEntryDto(
+          'Team$index',
+          randomGenerator.nextInt(100),
+          randomGenerator.nextInt(100),
+          randomGenerator.nextInt(100),
+          randomGenerator.nextInt(100),
+          randomGenerator.nextInt(100),
+          randomGenerator.nextInt(100),
+        );
 
-    final response = await client.get(getAllAgeGroupsUri, headers: headers);
+        return result;
+      },
+    );
 
-    if (response.statusCode == 200) {
-      var json = jsonDecode(response.body);
+    resultList.sort(
+      (a, b) => a.points.compareTo(b.points),
+    );
 
-      if (json is List<Map<String, dynamic>>) {
-        return json.map((e) => AgeGroupDto.fromJson(e)).toList();
-      }
-    }
-
-    return [];
+    return ResultsDto(1)
+      ..leagueResults = List.generate(
+        3,
+        (index) {
+          return resultleague.LeagueDto(index + 1)
+            ..gameResults = resultList.reversed.toList();
+        },
+      );
   }
 
-  List<AgeGroupDto> getAllAgeGroupsTestData() {
-    return [
-      AgeGroupDto('Altersklasse 1'),
-      AgeGroupDto('Altersklasse 2'),
-      AgeGroupDto('Altersklasse 3'),
-    ];
+  @override
+  Future<MatchScheduleDto?> getSchedule(String ageGroup) async {
+    int fieldCount = 1;
+    int teamCount = 1;
+    int hourCount = 10;
+    int timeCount = 10;
+
+    var scheduleList = List.generate(
+      10,
+      (innerIndex) {
+        var result = MatchScheduleEntryDto(
+          "$fieldCount",
+          "team${teamCount++}",
+          "team${teamCount++}",
+          "$hourCount:$timeCount Uhr",
+        );
+
+        fieldCount++;
+        if (fieldCount > 3) {
+          fieldCount = 1;
+        }
+
+        if (teamCount > 4) {
+          teamCount = 1;
+        }
+
+        timeCount += 10;
+        if (timeCount >= 60) {
+          timeCount = 10;
+          hourCount++;
+        }
+
+        return result;
+      },
+    );
+
+    return MatchScheduleDto(1)
+      ..leagueSchedules = List.generate(
+        8,
+        (index) {
+          return LeagueDto(index + 1)..scheduledGames = scheduleList;
+        },
+      );
+  }
+
+  @override
+  Future<bool?> startCurrentGames() async {
+    return true;
+  }
+
+  @override
+  Future<bool?> startNextRound() async {
+    return true;
   }
 }
