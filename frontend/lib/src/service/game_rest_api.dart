15928109374fa3f1e6491541fac9aa9b1b3b21c2
fseitz <fabi.seitz@gmail.com>
2025-03-23 21:49:29 +0100

    fix pdf

--- frontend/lib/src/service/game_rest_api.dart
+++ frontend/lib/src/service/game_rest_api.dart
@@ -265,15 +265,7 @@ class GameRestApiImplementation extends RestClient implements GameRestApi {
       if (response.statusCode == 200) {
         String fileName = 'Schiedsrichterzettel_Platz_$pitchId.pdf';
 
-        // remove quotes to make it a valid base64 string
-        var result = response.body.replaceAll('"', '');
-        var deecoded = base64Decode(result);
-
-        if (deecoded.isEmpty) {
-          return false;
-        }
-
-        final stream = Stream.fromIterable(deecoded);
+        final stream = Stream.fromIterable(response.bodyBytes);
         await download(stream, fileName);
         return true;
       }
