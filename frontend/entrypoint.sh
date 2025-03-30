6d1e1c95542ba3e02578f8ae36409e8f3f892ecd
Arne Maier <Arne_Maier@gmx.de>
2025-03-30 21:39:01 +0200

    configure backend url

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -4,7 +4,7 @@
 mkdir -p /usr/share/nginx/html/assets/assets/textfiles
 
 # Write token to file
-#echo "$CRYPTION_KEY" > /usr/share/nginx/html/assets/assets/textfiles/token.txt
+echo "$BACKEND_URL" > /usr/share/nginx/html/assets/assets/textfiles/backend-url.txt
 
 # Start nginx in foreground
 nginx -g 'daemon off;'
