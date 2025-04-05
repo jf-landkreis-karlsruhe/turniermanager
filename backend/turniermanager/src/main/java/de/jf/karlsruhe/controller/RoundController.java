cb8418f1e43f21b25d6b9dd44758771799ebd70f
Arne Maier <Arne_Maier@gmx.de>
2025-04-05 21:20:55 +0200

    Create pdf of result cards for a round

---
+++ backend/turniermanager/src/main/java/de/jf/karlsruhe/controller/RoundController.java
@@ -0,0 +1,180 @@
+package de.jf.karlsruhe.controller;
+
+import com.lowagie.text.*;
+import com.lowagie.text.Font;
+import com.lowagie.text.alignment.HorizontalAlignment;
+import com.lowagie.text.pdf.BaseFont;
+import com.lowagie.text.pdf.PdfWriter;
+import de.jf.karlsruhe.model.base.Game;
+import de.jf.karlsruhe.model.base.Round;
+import de.jf.karlsruhe.model.repos.GameRepository;
+import de.jf.karlsruhe.model.repos.RoundRepository;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.awt.*;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/rounds")
+@AllArgsConstructor
+public class RoundController {
+
+    private final RoundRepository roundRepository;
+    private final GameRepository gameRepository;
+
+
+    @GetMapping("/")
+    public ResponseEntity<List<RoundDTO>> getAllRounds() {
+        return ResponseEntity.ok(
+            roundRepository.findAll().stream()
+                .map(round -> new RoundDTO(round.getId(), round.getName(), round.getTournament().getId(), round.isActive()))
+                .toList()
+        );
+    }
+
+    @GetMapping(path = "/{id}/result-cards", produces = MediaType.APPLICATION_PDF_VALUE)
+    public ResponseEntity<byte[]> getResultCards(@PathVariable UUID id) {
+        Round round = roundRepository.findById(id).orElse(null);
+        if (round == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        List<Game> games = gameRepository.findByRound(round).stream()
+                .sorted(Comparator.comparing(Game::getGameNumber)).toList();
+
+        // TODO:
+        if (games.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        Map<UUID,List<Game>> gamesPerPitch = new HashMap<>();
+        for (Game game : games) {
+            gamesPerPitch.computeIfAbsent(game.getPitch().getId(), k -> new ArrayList<>()).add(game);
+        }
+
+        List<UUID> pitchByLength = gamesPerPitch.keySet().stream()
+                .sorted(Comparator.comparingInt(o -> gamesPerPitch.get(o).size()))
+                .toList();
+
+
+        try {
+            ByteArrayOutputStream out = createPdf(gamesPerPitch, pitchByLength);
+            HttpHeaders headers = new HttpHeaders();
+            headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=spielfeld" + id + ".pdf");
+            return ResponseEntity.ok().headers(headers).body(out.toByteArray());
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
+
+    private ByteArrayOutputStream createPdf(Map<UUID, List<Game>> games, List<UUID> pitchList) {
+        try {
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            Document document = new Document(PageSize.A4);
+            PdfWriter writer = PdfWriter.getInstance(document, out);
+            writer.setCloseStream(false);
+            document.open();
+
+
+            for (int pitchCount = 0; pitchCount < pitchList.size(); pitchCount = pitchCount + 2) {
+                List<Game> topGames = games.get(pitchList.get(pitchCount));
+                List<Game> bottomGames = pitchCount + 1 < pitchList.size() ? games.get(pitchList.get(pitchCount + 1)) : List.of();
+
+                for (int gameNumber = 0; gameNumber < topGames.size(); gameNumber++) {
+                    Game topgame = topGames.get(gameNumber);
+                    addGame(topgame, document);
+                    Paragraph divider = new Paragraph("\n------------------------------------------------------------------------------------------------");
+                    divider.setSpacingAfter(30f);
+                    divider.setAlignment(Element.ALIGN_CENTER);
+                    document.add(divider);
+
+                    if (gameNumber + 1 < bottomGames.size()) {
+                        Game bottomgame = bottomGames.get(gameNumber + 1);
+                        addGame(bottomgame, document);
+                    } else {
+                        addGame(topgame, document);
+                    }
+                    document.newPage();
+
+                }
+            }
+
+            document.close();
+            return out;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void addGame(Game game, Document document) throws DocumentException, IOException {
+        Font headlineFont = new Font(
+                BaseFont.createFont(BaseFont.HELVETICA_BOLD, BaseFont.CP1252, BaseFont.NOT_EMBEDDED),
+                18F,
+                Font.BOLD,
+                Color.BLACK
+        );
+        Font boldFont = new Font(
+                BaseFont.createFont(BaseFont.HELVETICA_BOLD, BaseFont.CP1252, BaseFont.NOT_EMBEDDED),
+                14F,
+                Font.BOLD,
+                Color.BLACK
+        );
+
+        Paragraph header = new Paragraph("Platz: " + game.getPitch().getName() + "    Spiel: " + game.getGameNumber(), headlineFont);
+        header.setAlignment(Element.ALIGN_CENTER);
+        document.add(header);
+
+        Table table = new Table(2);
+        table.setBorderWidth(1F);
+        table.setBorderColor(new Color(0, 0, 0));
+        table.setPadding(5F);
+        table.setWidth(100);
+        table.addCell(createCell(game.getTeamA().getName() + "\n\n\n\n\n"));
+        table.addCell(createCell(game.getTeamB().getName()));
+        table.endHeaders();
+        table.addCell(createCell(game.getTeamB().getName() + "\n\n\n\n\n"));
+        table.addCell(createCell(game.getTeamA().getName()));
+        document.add(table);
+
+        document.add(new Paragraph("Endergebnis:", headlineFont));
+        document.add(new Paragraph(game.getTeamA().getName() + ": ________________,     " + game.getTeamB().getName() + ": ________________", boldFont));
+    }
+
+    private static Cell createCell(String text) throws IOException {
+        Font font = new Font(
+                BaseFont.createFont(BaseFont.HELVETICA_BOLD, BaseFont.CP1252, BaseFont.NOT_EMBEDDED),
+                13F,
+                Font.BOLD,
+                Color.BLACK
+        );
+        Paragraph paragraph = new Paragraph(text, font);
+        paragraph.setAlignment(Element.ALIGN_CENTER);
+        Cell cell = new Cell(paragraph);
+        cell.setHorizontalAlignment(HorizontalAlignment.CENTER);
+        return cell;
+    }
+
+
+    @Data
+    @AllArgsConstructor
+    public static class RoundDTO {
+        private UUID id;
+        private String name;
+        private UUID tournamentId;
+        private boolean active;
+
+        // Getters and Setters
+    }
+}
