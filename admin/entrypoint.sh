332a018efa8af37cb7720e28d9e32fd59987a571
Arne Maier <Arne_Maier@gmx.de>
2025-04-06 15:01:11 +0200

    build and use docker

--- admin/entrypoint.sh
+++ admin/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 rm /usr/share/nginx/html/apiBaseApi.js
-echo "BASE_API_URL = '$BACKEND_URL';" > /usr/share/nginx/html/apiBaseApi.js
+echo "BASE_API_URL = 'http://$BACKEND_URL';" > /usr/share/nginx/html/apiBaseApi.js
 
 # Start nginx in foreground
 nginx -g 'daemon off;'
